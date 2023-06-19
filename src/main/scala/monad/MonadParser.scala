ed668e828d8407efca75372476307549284f5afa
19842639 <urzhenaliev@sberbank.ru>
2023-06-19 20:14:31 +0300

    MonadParser

---
+++ src/main/scala/monad/MonadParser.scala
@@ -0,0 +1,53 @@
+package monad
+
+class MonadParser[T, Src](private val p: Src => (T, Src)) {
+  def flatMap[M](f: T => MonadParser[M, Src]): MonadParser[M, Src] = MonadParser { src =>
+    val (word, rest) = p(src)
+    val monad = f(word)
+    val result = monad.p(rest)
+    result
+  }
+
+  def map[M](f: T => M): MonadParser[M, Src] = MonadParser { src =>
+    val (word, rest) = p(src)
+    (f(word), rest)
+  }
+
+  def parse(src: Src): T = p(src)._1
+}
+
+object MonadParser {
+  def apply[T, Src](f: Src => (T, Src)) = new MonadParser[T, Src](f)
+}
+
+object TestExecution {
+  def main(args: Array[String]): Unit = {
+    val str = "1997;Ford;G;true\n1901;Ford;T;false"
+
+    case class Car(year: Int, mark: String, model: String, canDrive: Boolean)
+
+    def StringField: MonadParser[String, String] = MonadParser[String, String] { str =>
+      val idx = str.indexOf(";")
+      if (idx > -1)
+        (str.substring(0, idx), str.substring(idx + 1))
+      else
+        (str, "")
+    }
+
+    def IntField: MonadParser[Int, String] = StringField.map(_.toInt)
+
+    def BooleanField: MonadParser[Boolean, String] = StringField.map(_.toBoolean)
+
+    val parser = for {
+      year <- IntField
+      mark <- StringField
+      model <- StringField
+      canDrive <- BooleanField
+    } yield Car(year, mark, model, canDrive)
+
+    val result = str.split('\n').map(parser.parse)
+
+    Console.println(result)
+
+  }
+}
